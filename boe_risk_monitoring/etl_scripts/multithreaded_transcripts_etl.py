8514b9c341bccd29c9f70ce66ad4043d251c8166
Zahin Rahim <zahinrahim03@gmail.com>
2025-06-13 14:29:38 +0100

    latest updates

---
+++ boe_risk_monitoring/etl_scripts/multithreaded_transcripts_etl.py
@@ -0,0 +1,72 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import os
+
+from boe_risk_monitoring.etl_scripts.etl_classes import TranscriptETL
+from boe_risk_monitoring.utils.utils import construct_input_output_file_paths
+import boe_risk_monitoring.config as config
+
+DATA_FOLDER = config.DATA_FOLDER
+
+def process_transcript(input_pdf_path, output_dir_path, llm_backend="gemini", llm_model_name="gemini-2.5-pro-preview-06-05"):
+	if not isinstance(input_pdf_path, str):
+		raise TypeError("Input path must be of type str.")
+
+	if not isinstance(output_dir_path, str):
+		raise TypeError("Output directory path must be of type str.")
+
+	# Identify if this is a Q4 presentation
+	if "Q4" in os.path.basename(input_pdf_path):
+		print(f"Identified {input_pdf_path} as a Q4 transcript.")
+		is_q4_transcript = True
+	else:
+		print(f"Identified {input_pdf_path} as a non-Q4 transcript.")
+		is_q4_transcript = False
+
+	# Instantiate the TranscriptETL class
+	transcript_etl = TranscriptETL(
+	    input_pdf_path=input_pdf_path,
+		is_q4_transcript=is_q4_transcript,
+	)
+
+	# Run the extract method
+	extracted_text = transcript_etl.extract()
+
+	# Run the transform method
+	chunks_df = transcript_etl.transform(
+	    raw_data=extracted_text,
+	    llm_backend=llm_backend,
+	    llm_model_name=llm_model_name,
+	    )
+
+	# Run the load method
+	transcript_etl.load(
+	    transformed_data=chunks_df,
+	    output_dir_path=output_dir_path,
+	    )
+
+if __name__ == "__main__":
+	# Get the input and output file paths
+	input_pdf_paths, output_dirs = construct_input_output_file_paths(data_dir=DATA_FOLDER, input_file_type="transcripts", skip_if_output_exists=True)
+	print(f"Found {len(input_pdf_paths)} transcript files to process.")
+
+	# # Create a ThreadPoolExecutor to process files concurrently
+	# with ThreadPoolExecutor(max_workers=4) as executor:
+	# 	futures = {}
+	# 	for input_pdf_path, output_dir_path in zip(input_pdf_paths, output_dirs):
+	# 		future = executor.submit(
+	# 			process_transcript,
+	# 			input_pdf_path=input_pdf_path,
+	# 			output_dir_path=output_dir_path,
+	# 			llm_backend="gemini",
+	# 			llm_model_name="gemini-2.5-pro-preview-06-05"
+	# 		)
+	# 		futures[future] = input_pdf_path
+
+	# 	# Wait for all futures to complete
+	# 	for future in as_completed(futures):
+	# 		try:
+	# 			future.result()  # This will raise an exception if the processing failed
+	# 		except Exception as e:
+	# 			print(f"Error processing {futures[future]}: {e}")
+
+
--- boe_risk_monitoring/llms/chunking_llm.py
+++ boe_risk_monitoring/llms/chunking_llm.py
@@ -8,19 +8,21 @@ from google import genai
 from boe_risk_monitoring.llms.base_llm import BaseLLM, SUPPORTED_LLMS_DICT
 
 class ChunkingLLM(BaseLLM):
-	def __init__(self, chunks_schema, metadata_schema, backend="openai", model_name="gpt-4.1", temperature=0.3):
+	def __init__(self, chunking_prompt, response_schema, backend="openai", model_name="gpt-4.1", temperature=0.3):
 		super().__init__(backend=backend, model_name=model_name, temperature=temperature)
-		self.chunks_schema = chunks_schema
-		self.metadata_schema = metadata_schema
+		if not isinstance(chunking_prompt, str):
+			raise TypeError("Chunking prompt must be a string.")
+		self.chunking_prompt = chunking_prompt
+		self.response_schema = response_schema
 
-	def invoke(self, prompt, response_schema):
+	def invoke(self):
 		if self.backend == "openai":
 			client = OpenAI(api_key=self.api_key)
 			resp = client.responses.parse(
 				model=self.model_name,
-				input=prompt,
+				input=self.chunking_prompt,
 				temperature=self.temperature,
-				text_format=response_schema,
+				text_format=self.response_schema,
 			)
 			return resp.output_parsed
 
@@ -28,10 +30,10 @@ class ChunkingLLM(BaseLLM):
 			client = genai.Client(api_key=self.api_key)
 			resp = client.models.generate_content(
 				model=self.model_name,
-				contents=prompt,
+				contents=self.chunking_prompt,
 				config={
 					"response_mime_type": "application/json",
-					"response_schema": response_schema,
+					"response_schema": self.response_schema,
 				}
 			)
 			return resp.parsed
@@ -39,74 +41,17 @@ class ChunkingLLM(BaseLLM):
 		else:
 			raise ValueError(f"Unsupported backend: {self.backend}")
 
-	def _make_doc_metadata_prompt(self, text):
-		return (
-			"You are an assistant that extracts structured metadata from financial earnings call transcripts."
-			"From the text below, extract the following metadata:"
-			"reporting year: the year of the reporting period."
-			"reporting quarter: the quarter of the reporting period."
-			"date: the date the earnings call occurred."
-			f"Text:\n{text}"
-		)
 
-	def _make_chunk_prompt(self, text):
-		return (
-			"You are an expert in document structuring and analysis. "
-			"Split the following financial earnings transcript into **small, logically coherent chunks**. "
-			"Each chunk should be **no more than one paragraph**, and ideally a single idea or statement. "
-			"Break chunks at changes in **speaker**, **topic**, or shifts in financial focus. "
-			"If a paragraph contains multiple ideas, split it into multiple smaller chunks. "
-			"Preserve the original order of text.\n"
-			# "Ensure clarity and minimalism in each chunk. Avoid combining multiple statements into one block.\n\n"
-			# "Additionally, for each chunk, provide the following:\n:"
-			# "1. flag if the chunk contains a forward-looking statement (e.g., projections, outlook, or future plans)\n"
-			# "Example:\n"
-			# "Chunk: We expect revenues to grow 10% next quarter."
-			# "forward_looking: true\n"
-			# "Chunk: Revenue this quarter was $2.3 billion, a 5% increase year-over-year."
-			# "forward_looking: false\n"
-			# "Only flag if there is a clear statement about the future\n."
-			# "2. List out any mentions of external or macroeconomic factors. Omit if not present.\n"
-			# "Examples of macro factors include: interest rates, inflation, monetary policy, regulatory changes, geopolitical instability, FX rates, tariffs, etc.\n"
-			# "If the factor doesn't fit any known category, describe it clearly (e.g. tariffs)\n"
-			f"Transcript:\n{text}"
-		)
 
-	def estimate_required_pre_chunks(self, text):
-		# Approximate number of tokens of prompt + approximate response tokens
-		prompt = self._make_chunk_prompt(text)
-		n_words_prompt = len(prompt.split())
-		# 100 tokens to 75 words rule of thumb, applying a 1.2 safety factor too
-		n_tokens_prompt = int(n_words_prompt*4/3*1.2)
-		# We expect the output chunks to have a similar order for number of tokens as the input prompt but we add 10% for structured response characters (e.g. curly brackets), applying a 1.2 safety factor too
-		n_tokens_response = int(n_tokens_prompt*1.1*1.2)
-		# Total input and output tokens
-		input_output_tokens = (n_tokens_prompt + n_tokens_response)
-		# Check against context_window
-		context_window = self.context_window
-		pre_chunks_context_window = 0
-		pre_chunks_max_output = 0
-		if input_output_tokens > context_window:
-			print(f"Estimated prompt + response tokens = {input_output_tokens}. This exceeds the context window of {self.model_name} = {context_window}.")
-			pre_chunks_context_window = math.ceil(input_output_tokens/context_window)
-		max_output = self.max_output
-		if n_tokens_response > max_output:
-			print(f"Estimated response tokens = {n_tokens_response}. This exceeds the max output tokens of {self.model_name} = {max_output}.")
-			pre_chunks_max_output = math.ceil(n_tokens_response/max_output)
-		return pre_chunks_context_window, pre_chunks_max_output
 
-	def fetch_metadata(self, text, metadata_schema=None):
-		if not metadata_schema:
-			metadata_schema = self.metadata_schema
-		prompt = self._make_doc_metadata_prompt(text)
-		return self.invoke(prompt, metadata_schema)
+	# def fetch_metadata(self, text, metadata_schema=None):
+	# 	if not metadata_schema:
+	# 		metadata_schema = self.metadata_schema
+	# 	prompt = self._make_doc_metadata_prompt(text)
+	# 	return self.invoke(prompt, metadata_schema)
 
-	def chunk_transcript(self, text, chunks_schema=None):
-		if not chunks_schema:
-			chunks_schema = self.chunks_schema
-		prompt = self._make_chunk_prompt(text)
-		return self.invoke(prompt, chunks_schema)
-
-	# async def chunk_transcript_async(self, text):
-	#     prompt = self._make_chunk_prompt(text)
-	#     return await self.ainvoke(prompt)
+	# def chunk_transcript(self, text, chunks_schema=None):
+	# 	if not chunks_schema:
+	# 		chunks_schema = self.chunks_schema
+	# 	prompt = self._make_chunk_prompt(text)
+	# 	return self.invoke(prompt, chunks_schema)
