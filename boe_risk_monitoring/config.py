2bafaca37ab2c2e6cc72df0d950a7807e03bc7c8
zr03 <zahin.rahim@ukpowernetworks.co.uk>
2025-06-23 23:39:31 +0100

    integrating Alexandra's share price ETL

--- boe_risk_monitoring/config.py
+++ boe_risk_monitoring/config.py
@@ -2,3 +2,16 @@ import os
 
 DATA_FOLDER = "data"
 AGGREGATED_DATA_FOLDER_NAME = "aggregated"
+SHARE_PRICE_HISTORY_START_DATE = "2022-01-01"
+PERMISSIBLE_BANK_NAMES = ["citigroup", "jpmorgan", "bankofamerica"] # These match the folder names in the data directory
+BANK_NAME_MAPPING = {
+    "citigroup": "Citigroup",
+    "jpmorgan": "JPMorgan",
+    "bankofamerica": "Bank of America",
+}
+
+TICKER_MAPPING = {
+    "citigroup": "C",
+    "jpmorgan": "JPM",
+    "bankofamerica": "BAC",
+}
