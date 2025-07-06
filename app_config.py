83dcace403c30d31302566eee9f0a25e41547e2d
Silvia Sottini <silvia.sottini2@gmail.com>
2025-07-06 11:06:22 +0100

    added data

--- app_config.py
+++ app_config.py
@@ -4,4 +4,5 @@ DATA_FOLDER = os.path.join("data", "app")
 TOPIC_RELEVANCE_Q_AGG_NORM_FPATH = os.path.join(DATA_FOLDER, "multi_topic_modelling_with_relevance_quarter_agg_norm.parquet")
 TOPIC_SENTIMENT_Q_AGG_NORM_FPATH = os.path.join(DATA_FOLDER, "multi_topic_modelling_with_relevance_sentiment_quarter_agg_norm.parquet")
 Q_A_ANALYSIS_FPATH = os.path.join(DATA_FOLDER, "data_qa_pairs.parquet")
-
+SENTIMENT_SCORE_PATH=os.path.join(DATA_FOLDER,"multi_topic_modelling_with_relevance_sentiment_quarter_agg_norm.parquet")
+BANK_METRICS_PATH=os.path.join(DATA_FOLDER,"financial_metrics.parquet")
